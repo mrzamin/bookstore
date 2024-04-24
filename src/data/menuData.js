d91774dda0a7125768d9e7d6c47a6fd5453f16f5
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-04-24 12:55:00 -0400

    Add initial menu data

---
+++ src/data/menuData.js
@@ -0,0 +1,94 @@
+export const menuData = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Shop All",
+    href: "/shop",
+  },
+  {
+    label: "Personal Growth",
+    href: "/personalgrowth",
+    children: [
+      {
+        heading: "Mindset",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          {
+            label: "Book 2",
+            href: "#",
+          },
+          { label: "Book 3", href: "#" },
+        ],
+      },
+      {
+        heading: "Goal Setting & Achieving",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          {
+            label: "Book 2",
+            href: "#",
+          },
+          { label: "Book 3", href: "#" },
+        ],
+      },
+      {
+        heading: "Motivation, Inspiration, & Success",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          {
+            label: "Book 2",
+            href: "#",
+          },
+          { label: "Book 3", href: "#" },
+        ],
+      },
+
+      {
+        heading: "Communication and Relationships",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          {
+            label: "Book 2",
+            href: "#",
+          },
+          { label: "Book 3", href: "#" },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Entrepreneurship",
+    href: "/entrepreneurship",
+    children: [
+      {
+        heading: "Entrepreneurship",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          { label: "Book 2", href: "#" },
+          { label: "Book 3", href: "#" },
+          { label: "See all Entrepreneurship", href: "#" },
+        ],
+      },
+      {
+        heading: "Leadership",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          { label: "Book 2", href: "#" },
+          { label: "Book 3", href: "#" },
+          { label: "See all Leadership", href: "#" },
+        ],
+      },
+      {
+        heading: "Communication",
+        submenu: [
+          { label: "Book 1", href: "#" },
+          { label: "Book 2", href: "#" },
+          { label: "Book 3", href: "#" },
+          { label: "See all Communication", href: "#" },
+        ],
+      },
+    ],
+  },
+];
