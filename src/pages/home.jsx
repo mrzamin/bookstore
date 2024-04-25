12740acb8a1b9912f9e2f6e3484c27a31decd55e
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-04-24 21:28:35 -0400

    Add Carousel to Home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,18 @@ import HeroContent from "../components/Hero/HeroContent";
 import HeroTitle from "../components/Hero/HeroTitle";
 
 import hero from "../../public/images/pastel-abstract.png";
+import Carousel from "../components/Carousel/Carousel";
 
 const HomePage = () => {
   return (
-    <Hero>
-      <HeroContent imageSrc={hero}>
-        <HeroTitle title="The Mindset Bookstore" />
-      </HeroContent>
-    </Hero>
+    <>
+      <Hero>
+        <HeroContent imageSrc={hero}>
+          <HeroTitle title="The Mindset Bookstore" />
+        </HeroContent>
+      </Hero>
+      <Carousel />
+    </>
   );
 };
 
