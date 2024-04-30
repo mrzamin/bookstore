069fedf7704f817811bfc963a53e0359258842e7
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-04-30 19:44:21 -0400

    Add number of items badge and related CSS for cart icon

--- src/components/Navbar/Cart.jsx
+++ src/components/Navbar/Cart.jsx
@@ -1,8 +1,16 @@
 import { UserRoundX } from "lucide-react";
 import { Link } from "react-router-dom";
 import cartIcon from "../../../public/icons/cart.png";
+import { useContext } from "react";
+import { StoreContext } from "../../Context";
 
 const Cart = () => {
+  const { products, addToCart, removeFromCart } = useContext(StoreContext);
+
+  const inCart = products.filter((product) => product.inBag === true);
+
+  const inCartTotal = inCart.reduce((total, book) => total + book.quantity, 0);
+
   return (
     <div className="register">
       <div className="block md:hidden">
@@ -10,10 +18,15 @@ const Cart = () => {
           <UserRoundX />
         </button>
       </div>
-      <div className="hidden md:block">
+      <div className="hidden md:block navCart">
         <Link to="/checkout" className="cart">
           <div>Cart</div>
           <img src={cartIcon} width="40px" alt="cart" />
+          {inCart.length > 0 && (
+            <div className="itemsInCart">
+              <span>{inCartTotal}</span>
+            </div>
+          )}
         </Link>
       </div>
     </div>
