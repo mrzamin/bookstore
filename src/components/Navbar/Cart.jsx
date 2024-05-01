59fd46c2a1790fa89f38ac11acdae58722bbe41c
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-01 13:20:01 -0400

    Add tooltip config and styles

--- src/components/Navbar/Cart.jsx
+++ src/components/Navbar/Cart.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import cartIcon from "../../../public/icons/cart.png";
 import { useContext } from "react";
 import { StoreContext } from "../../Context";
+import CartTooltip from "./MegaMenu/Cart Tooltip/CartTooltip";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import styles from "../Navbar/MegaMenu/Cart Tooltip/CartTooltip.module.css";
 
 const Cart = () => {
   const { products, addToCart, removeFromCart } = useContext(StoreContext);
@@ -14,12 +18,12 @@ const Cart = () => {
   return (
     <div className="register">
       <div className="block md:hidden">
-        <button className="avatar">
+        <div className="avatar">
           <UserRoundX />
-        </button>
+        </div>
       </div>
       <div className="hidden md:block navCart">
-        <Link to="/checkout" className="cart">
+        <Link to="/checkout" data-tooltip-id="tooltip" className="cart">
           <div>Cart</div>
           <img src={cartIcon} width="40px" alt="cart" />
           {inCart.length > 0 && (
@@ -28,6 +32,16 @@ const Cart = () => {
             </div>
           )}
         </Link>
+        <Tooltip
+          style={{ backgroundColor: "white" }}
+          id="tooltip"
+          className={styles.tooltip}
+          clickable={true}
+          place="bottom-end"
+          opacity={1}
+        >
+          <CartTooltip inCart={inCart} />
+        </Tooltip>
       </div>
     </div>
   );
---
+++ src/components/Navbar/MegaMenu/Cart Tooltip/CartTooltip.jsx
@@ -0,0 +1,58 @@
+import styles from "./CartTooltip.module.css";
+import { useContext } from "react";
+import { StoreContext } from "../../../../Context";
+import deleteIcon from "../../../../../public/icons/deleteIcon.png";
+
+const CartToolTip = ({ inCart }) => {
+  console.log(inCart);
+  const { products, addToCart, removeFromCart } = useContext(StoreContext);
+
+  const handleDelete = (id) => {
+    const product = products.find((prod) => prod.id === id);
+
+    removeFromCart(product);
+  };
+  return (
+    <div className={styles.cartTooltip}>
+      <h2>Cart</h2>
+      {inCart.length > 0 ? (
+        <div className={styles.bookList}>
+          {inCart.map((book) => (
+            <div className={styles.bookItem} key={book.id}>
+              <div className={styles.leftContainer}>
+                <div className={styles.imageContainer}>
+                  <img
+                    className={styles.image}
+                    src={book.src}
+                    alt="book-cover"
+                  />
+                </div>
+                <div className={styles.productInformation}>
+                  <h4 className={styles.title}>{book.title}</h4>
+                  <div className={styles.quantity}>Qty: {book.quantity}</div>
+                </div>
+                <div className={styles.deleteContainer}>
+                  <img
+                    className={styles.deleteIcon}
+                    src={deleteIcon}
+                    onClick={(e) => {
+                      handleDelete(book.id);
+                    }}
+                    alt="delete"
+                  />
+                </div>
+              </div>
+              <div className={styles.rightContainer}>
+                <h5>${(book.price * book.quantity).toFixed(2)}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.emptyCart}>Your cart is empty.</div>
+      )}
+    </div>
+  );
+};
+
+export default CartToolTip;
