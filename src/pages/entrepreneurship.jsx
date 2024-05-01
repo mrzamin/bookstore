15bca62a4cb70567874136a39f96597c36ca9d6b
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-01 13:22:47 -0400

    Add Book Preview Component

--- src/pages/entrepreneurship.jsx
+++ src/pages/entrepreneurship.jsx
@@ -1,16 +1,27 @@
 import Hero from "../components/Hero/Hero";
 import HeroContent from "../components/Hero/HeroContent";
 import HeroTitle from "../components/Hero/HeroTitle";
-
+import { useContext } from "react";
+import { StoreContext } from "../Context.jsx";
+import BookPreview from "../components/Books/BookPreview/BookPreview";
 import hero from "../../public/images/pastel-abstract.png";
 
 const EntrepreneurshipPage = () => {
+  const { products } = useContext(StoreContext);
+
   return (
-    <Hero>
-      <HeroContent imageSrc={hero}>
-        <HeroTitle title="Entrepreneurship Books" />
-      </HeroContent>
-    </Hero>
+    <>
+      <Hero>
+        <HeroContent imageSrc={hero}>
+          <HeroTitle title="Entrepreneurship Books" />
+        </HeroContent>
+      </Hero>
+      <BookPreview
+        products={products.filter(
+          (product) => product.category === "entrepreneurship"
+        )}
+      ></BookPreview>
+    </>
   );
 };
 
--- src/pages/personalgrowth.jsx
+++ src/pages/personalgrowth.jsx
@@ -1,16 +1,26 @@
 import Hero from "../components/Hero/Hero";
 import HeroContent from "../components/Hero/HeroContent";
 import HeroTitle from "../components/Hero/HeroTitle";
-
+import { useContext } from "react";
+import { StoreContext } from "../Context.jsx";
+import BookPreview from "../components/Books/BookPreview/BookPreview";
 import hero from "../../public/images/pastel-abstract.png";
 
 const PersonalGrowthPage = () => {
+  const { products } = useContext(StoreContext);
   return (
-    <Hero>
-      <HeroContent imageSrc={hero}>
-        <HeroTitle title="Personal Growth Books" />
-      </HeroContent>
-    </Hero>
+    <>
+      <Hero>
+        <HeroContent imageSrc={hero}>
+          <HeroTitle title="Personal Growth Books" />
+        </HeroContent>
+      </Hero>
+      <BookPreview
+        products={products.filter(
+          (product) => product.category === "personal growth"
+        )}
+      ></BookPreview>
+    </>
   );
 };
 
