df094a0bf046c22d39cb43cd91462495da4ae09c
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-05 15:16:13 -0400

    Update component imports and routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import ShopPage from "./pages/ShopPage/ShopPage.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import BookDetailPage from "./pages/BookDetailPage/BookDetailPage.jsx";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage.jsx";
 
-import Layout from "./pages/layout";
-import ErrorPage from "./pages/error";
-import ShopPage from "./pages/Shop/shop.jsx";
-import PersonalGrowthPage from "./pages/personalgrowth";
-import EntrepreneurshipPage from "./pages/entrepreneurship";
-import HomePage from "./pages/Home/home.jsx";
-import BookDetail from "./components/Books/BookDetail/BookDetail";
-import Checkout from "./components/Checkout/Checkout";
+import MindsetPage from "./pages/topic pages/MindsetPage.jsx";
+import GoalSettingPage from "./pages/topic pages/GoalSettingandHabitsPage.jsx";
+import MotivationPage from "./pages/topic pages/MotivationPage.jsx";
+import EntrepreneurshipPage from "./pages/topic pages/EntrepreneurshipPage.jsx";
+import LeadershipPage from "./pages/topic pages/LeadershipPage.jsx";
+import CommunicationPage from "./pages/topic pages/CommunicationPage.jsx";
+import DailyCommunicationPage from "./pages/topic pages/DailyCommunicationPage.jsx";
+import PublicSpeakingPage from "./pages/topic pages/PublicSpeakingPage.jsx";
+import MoneyPage from "./pages/topic pages/MoneyPage.jsx";
+import FinancialFreedomPage from "./pages/topic pages/FinancialFreedomPage.jsx";
+import InvestingPage from "./pages/topic pages/InvestingPage.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,24 +33,76 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: "/shop",
+        path: "shop",
         element: <ShopPage />,
       },
       {
-        path: "/personalgrowth",
-        element: <PersonalGrowthPage />,
+        path: "shopbysubject",
+        element: <ShopPage />,
       },
       {
-        path: "/entrepreneurship",
+        path: "shop/:slug",
+        element: <BookDetailPage />,
+      },
+      {
+        path: "shop/:topic/:slug",
+        element: <BookDetailPage />,
+      },
+      {
+        path: "shopbysubject/:slug",
+        element: <BookDetailPage />,
+      },
+      {
+        path: "shop/mindsetbooks",
+        element: <MindsetPage />,
+      },
+      {
+        path: "shop/mindsetbooks/:slug",
+        element: <BookDetailPage />,
+      },
+
+      {
+        path: "shop/goalandhabitbooks",
+        element: <GoalSettingPage />,
+      },
+      {
+        path: "shop/motivationbooks",
+        element: <MotivationPage />,
+      },
+      {
+        path: "shop/entrepreneurshipbooks",
         element: <EntrepreneurshipPage />,
       },
       {
-        path: "/shop/:slug",
-        element: <BookDetail />,
+        path: "shop/leadershipbooks",
+        element: <LeadershipPage />,
       },
       {
-        path: "/checkout",
-        element: <Checkout />,
+        path: "shop/communicationbooks",
+        element: <CommunicationPage />,
+      },
+      {
+        path: "shop/dailycommunicationbooks",
+        element: <DailyCommunicationPage />,
+      },
+      {
+        path: "shop/publicspeakingbooks",
+        element: <PublicSpeakingPage />,
+      },
+      { path: "shop/moneybooks", element: <MoneyPage /> },
+
+      {
+        path: "shop/investingbooks",
+        element: <InvestingPage />,
+      },
+      {
+        path: "shop/financialfreedombooks",
+        element: <FinancialFreedomPage />,
+      },
+
+      {
+        path: "checkout",
+        element: <CheckoutPage />,
       },
     ],
   },
