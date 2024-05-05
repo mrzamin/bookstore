b3b7488869595c6e8b21e6a8917daa0e06a54147
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-05 15:02:04 -0400

    Update image path

--- src/utils/getImagePath.js
+++ src/utils/getImagePath.js
@@ -1,9 +1,6 @@
 const getImgPath = (author) => {
   const authorName = author.split(" ").reverse().join("").toString();
-
-  const imgPath = `../public/images/${authorName}.jpg`;
-
-  console.log(imgPath);
+  const imgPath = `../../../public/images/${authorName}.jpg`;
   return imgPath;
 };
 
