0a68c2c3f7771b0d080cd2202316c8fafc6eca58
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-05-05 21:57:55 -0400

    Update ErrorPage component styles

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,17 @@
 import { useRouteError, Link } from "react-router-dom";
 import styles from "./ErrorPage.module.css";
-
+import logo from "../../../public/icons/appLogo.png";
 const ErrorPage = () => {
-  const error = useRouteError();
   return (
     <div className={styles.errorPage}>
-      <h1>Oh no! No book here.</h1>
-      <p>That page cannot be found.</p>
-      <p>
-        <i>Error Message: {error.statusText || error.message}</i>
-      </p>
+      <div className={styles.errorDiv}>
+        <h1>Oh no! No book here.</h1>
+        <img src={logo} className={styles.logo} alt="book-logo" />
+        <p>That page cannot be found.</p>
 
-      <div>
-        Go back to the <Link to="/"> Homepage</Link>
+        <div>
+          Go back to the <Link to="/"> Homepage</Link>
+        </div>
       </div>
     </div>
   );
